26332ca4a58083df5cc98878ad8321403d6cf3fd
sfauvel <sfauvel@users.noreply.github.com>
2024-02-06 08:00:16 +0000

    deploy: 794aa5766f60fb9ef6b982bb29900785c5f50507

---
+++ doc/assets/js/5e95c892.6247c61d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmithril_doc=self.webpackChunkmithril_doc||[]).push([[9661],{41892:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});s(67294);var c=s(90512),t=s(1944),u=s(35281),a=s(18790),i=s(37293),l=s(85893);function d(e){return(0,l.jsx)(t.FG,{className:(0,c.Z)(u.k.wrapper.docsPages),children:(0,l.jsx)(i.Z,{children:(0,a.H)(e.route.routes)})})}}}]);
